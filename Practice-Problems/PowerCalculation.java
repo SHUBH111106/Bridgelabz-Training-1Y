8d6786ae9b0d2d15ab3f6f43e9bc064df528a6d3
SHUBH11106 <gargshubh061@gmail.com>
2026-01-14 23:27:31 +0530

    Add Pratice Problems

--- Practice-Problems/PowerCalculation.java
+++
@@ -1,9 +0,0 @@
-class PowerCalculation {
-    public static void main(String[] args) {
-        double work = 500;
-        double time = 10;
-
-        double power = work / time;
-        System.out.println("Power = " + power);
-    }
-}
--- Practice-Problems/SpeedCalculation.java
+++
@@ -1,9 +0,0 @@
-class SpeedCalculation {
-    public static void main(String[] args) {
-        double distance = 150; // km
-        double time = 2;       // hours
-
-        double speed = distance / time;
-        System.out.println("Speed = " + speed + " km/hr");
-    }
-}
