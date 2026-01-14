b37381ed32160ae018efa0b28b407620239e81d6
SHUBH11106 <gargshubh061@gmail.com>
2026-01-15 00:22:30 +0530

    Discount

---
+++ Practice-Problems/FeeDiscount.java
@@ -0,0 +1,12 @@
+class FeeDiscount {
+    public static void main(String[] args) {
+        double fee = 125000;
+        double discountPercent = 10;
+
+        double discount = fee * discountPercent / 100;
+        double finalFee = fee - discount;
+
+        System.out.println("Discount = " + discount);
+        System.out.println("Final Fee = " + finalFee);
+    }
+}
