3d9c9107e06e8559ab66374cfc51cef15e67471f
SHUBH11106 <gargshubh061@gmail.com>
2026-01-15 23:20:09 +0530

    PurchaseCalculator

---
+++ Basic-Programmes/PurchaseCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class PurchaseCalculator {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double unitPrice, totalPrice;
+        int quantity;
+
+        System.out.print("Enter unit price: ");
+        unitPrice = input.nextDouble();
+
+        System.out.print("Enter quantity: ");
+        quantity = input.nextInt();
+
+        totalPrice = unitPrice * quantity;
+
+        System.out.println("The total purchase price is INR "
+                + totalPrice + " if the quantity "
+                + quantity + " and unit price is INR "
+                + unitPrice);
+    }
+}
