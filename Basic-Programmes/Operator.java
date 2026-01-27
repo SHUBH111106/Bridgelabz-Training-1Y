1b2f75ece6a9cef6bdbe8de2a0488485bd93d1d4
SHUBH11106 <gargshubh061@gmail.com>
2026-01-27 23:41:07 +0530

    Assignment 3

---
+++ Basic-Programmes/Operator.java
@@ -0,0 +1,17 @@
+class Operator{
+	public static void main(String[] args){
+		for(int i = 0 ; i < 10 ; i++){
+			System.out.println("i : " +i);
+		}
+		int j = 10;
+		while(j>0){
+			System.out.println("j : " +j);
+			j--;
+		}
+		int k =10;
+		do{
+			System.out.println("k : " +k);
+			k--;
+		}while(k>0);
+	}
+}
